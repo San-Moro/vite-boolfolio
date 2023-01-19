832f7b09b985bc5bbc5aa51dc1b851e688ad44f1
San-Moro <valentinacortese.dsg@gmail.com>
2023-01-19 17:16:21 +0100

    add: milestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,23 @@
+<script>
+import AppMain from './components/AppMain.vue';
 
+export default {
+  name: 'App',
+  components: {
+    AppMain
+  }
+}
+</script>
+
+<template>
+
+  <main>
+    <AppMain />
+  </main>
+
+</template>
+
+<style lang="scss">
+@use "styles/general.scss" as *;
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,41 @@
+<script>
+import axios from "axios";
+
+export default {
+    name: "AppMain",
+    data() {
+        return {
+            baseUrl: "http://127.0.0.1:8000",
+            projects: []
+        }
+    },
+    created() {
+        this.getProjects();
+    },
+    methods: {
+        getProjects() {
+            axios.get(`${this.baseUrl}/api/projects`).then(resp=> {
+                this.projects = resp.data.results;
+            })
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="container text-center">
+        <h2 class="text-center mb-4">All our projects</h2>
+        <div class="row row-cols-3 flex-wrap justify-content-center g-3">
+            <div class="card mx-2" style="width: 18rem;" v-for="project in projects" :key="project.id">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ project.title }}</h5>
+                    <p class="card-text">{{ project.description }}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+           
+        </div>
+    </div>
+</template>
